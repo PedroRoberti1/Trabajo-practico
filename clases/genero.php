75fb0df62a3e9c65f7221aac09d32c542ae49452
Pedro Roberti <pedroroberti99@gmail.com>
2023-10-06 11:02:16 -0300

    creamos la validacion de credenciales y la clase genero

---
+++ clases/genero.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+class genero{
+
+        protected $codigo_genero;
+        protected $nombre;
+
+    public function __construct($codigo_genero, $nombre)
+    {
+        $this->codigo_genero=$codigo_genero;
+        $this->nombre=$nombre;
+    }
+
+    public function getCodigo_genero() {
+        return $this->codigo_genero;
+        }
+    
+        public function getNombre() {
+        return $this->nombre;
+        }
+
+
+
+}
+
+
+
