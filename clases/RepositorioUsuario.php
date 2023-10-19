5b32cb11ce25561e152aaa5e5678b43a947dc9b5
Andres Ramirez <41218275@terciariourquiza.edu.ar>
2023-10-19 15:30:48 -0300

    crear clase repositorio  y modificar los demas

--- clases/RepositorioUsuario.php
+++ clases/RepositorioUsuario.php
@@ -1,32 +1,9 @@
 <?php 
 require_once '.env.php';
 require_once 'Usuario.php';
+require_once 'Repositorio.php';
 
-class Repositorio_Usuario{
-
-	private static $conexion = null;
-
-	public function __construct(){
-
-		if (is_null(self::$conexion)) {
-			$credenciales = credenciales();
-			self::$conexion = new mysqli(
-				$credenciales['servidor'],
-				$credenciales['usuario'],
-				$credenciales['clave'],
-				$credenciales['base_de_datos']
-			);
-			if (self::$conexion->connect_error){
-				$error = 'Error al conectar:' . self::$conexion->connect_error;
-				self::$conexion = null;
-				die($error);
-			}
-			self::$conexion->set_charset('utf8mb4');
-		}
-	}
-
-
-	
+class Repositorio_Usuario extends Repositorio{
 
 	public function login($nombre_usuario, $clave){
 
